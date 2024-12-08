cd07e7e14dfd10b1a963de5f8b1f2692395a6adb
Matt Catalano <mattcat26@gmail.com>
2024-12-08 16:53:32 -0600

    couldn't quite get part 2, moving on to day 3 for now

--- Day2/Day2part2.py
+++ Day2/Day2part2.py
@@ -3,21 +3,31 @@
 from typing import List
 
 def is_safe(problem_damper: bool, lst: List) -> bool:
+	print(lst)
 	first = True
 	check1 = 0
 	check2 = 0
 
-	for j in lst:
+	for i, j in enumerate(lst):
 		if first:
 			current = j
 			first = False
 			continue
 		if abs(current - j) <= 3 and abs(current - j) >= 1:
 			check1 += 1
-		if current-j < 0:
-			check2 += 1
-		if current-j > 0:
-			check2 -= 1
+		elif problem_damper:
+			new_lst = lst[:i] + lst[i+1:]
+			return is_safe(False, new_lst)
+		if current-j < 0 or current-j > 0:
+			if current-j < 0:
+				check2 += 1
+			if current-j > 0:
+				check2 -= 1
+		if abs(check2) < i and problem_damper:
+			new_lst = lst[:i] + lst[i+1:]
+			print('new_lst 2')
+			print(new_lst)
+			return is_safe(False, new_lst)
 		current = j
 
 	if abs(check1) == len(lst)-1 and abs(check2) == len(lst)-1:
@@ -25,7 +35,9 @@ def is_safe(problem_damper: bool, lst: List) -> bool:
 	return False	
 
 
-with open("input.txt", "r") as file:
+
+
+with open("test_input.txt", "r") as file:
 	safe_count = 0
 	for i in file.readlines():
 
@@ -36,4 +48,3 @@ with open("input.txt", "r") as file:
 			safe_count += 1
 	
 	print(safe_count)
-			
