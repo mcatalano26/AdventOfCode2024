e04e5b85596e10dbc3f6484d91a6c3a8be5491b1
Matt Catalano <mattcat26@gmail.com>
2024-12-15 21:03:54 -0600

    clean up 2.2

--- Day2/p2.py
+++ Day2/p2.py
@@ -1,32 +1,32 @@
 # https://adventofcode.com/2024/day/2
 
-from typing import List
+from typing import List, Tuple
 
-def is_safe(row_number: int, problem_dampener: bool, lst: List) -> bool:
-	first = True
+def problem_dampener_run_setup(lst: List, ind: int) -> Tuple[List, List]:
+	lst1 = lst.copy()
+	lst2 = lst.copy()
+	lst1.pop(ind)
+	lst2.pop(ind-1)
+	return lst1, lst2
+
+def is_safe(problem_dampener: bool, lst: List) -> bool:
+	# check1 will check if our delta is at least 1 and at most 3
+	# check2 will check that we're always increasing or decreasing
 	check1 = 0
 	check2 = 0
 
 	for ind, curr in enumerate(lst):
-		if first:
+		# If we're at the value 0, we don't have any good information yet
+		if ind == 0:
 			prev = curr
-			first = False
 			continue
 		if abs(prev - curr) <= 3 and abs(prev - curr) >= 1:
 			check1 += 1
 		elif problem_dampener:
-			print(f"lst {lst} is not safe. Popping {lst[ind]}")
-			problem_dampener = False
-			lst1 = lst.copy()
-			lst2 = lst.copy()
-			lst1.pop(ind)
-			lst2.pop(ind-1)
-			if is_safe(row_number, problem_dampener, lst1):
-				return True
-			elif is_safe(row_number, problem_dampener, lst2):
-				return True
-			else:
-				return False
+			lst1, lst2 = problem_dampener_run_setup(lst, ind)
+			if is_safe(False, lst1): return True
+			elif is_safe(False, lst2): return True
+			else: return False
 		if prev-curr != 0:
 			if prev-curr < 0:
 				check2 += 1
@@ -34,60 +34,35 @@ def is_safe(row_number: int, problem_dampener: bool, lst: List) -> bool:
 				check2 -= 1
 			if abs(check2) < ind:
 				if problem_dampener:
-					print(f"lst {lst} is not safe. Popping {lst[ind]}")
-					problem_dampener = False
-					lst1 = lst.copy()
-					lst2 = lst.copy()
-					lst1.pop(ind)
-					lst2.pop(ind-1)
-					if is_safe(row_number, problem_dampener, lst1):
-						return True
-					elif is_safe(row_number, problem_dampener, lst2):
-						return True
-					else:
-						return False
+					lst1, lst2 = problem_dampener_run_setup(lst, ind)
+					if is_safe(False, lst1): return True
+					elif is_safe(False, lst2): return True
+					else: return False
 		elif problem_dampener:
-			print(f"lst {lst} is not safe. Popping {lst[ind]}")
-			problem_dampener = False
-			lst1 = lst.copy()
-			lst2 = lst.copy()
-			lst1.pop(ind)
-			lst2.pop(ind-1)
-			if is_safe(row_number, problem_dampener, lst1):
-				return True
-			elif is_safe(row_number, problem_dampener, lst2):
-				return True
-			else:
-				return False
+			lst1, lst2 = problem_dampener_run_setup(lst, ind)
+			if is_safe(False, lst1): return True
+			elif is_safe(False, lst2): return True
+			else: return False
 		prev = curr
 
 	if abs(check1) == len(lst)-1 and abs(check2) == len(lst)-1:
-		print(f"lst {lst} is safe. Problem dampener = {problem_dampener}. Row number = {row_number}")
 		return True
-	print(f"lst {lst} is not safe. Problem dampener = {problem_dampener}. Row number = {row_number}")
 	return False
 
 
 with open("input.txt", "r") as file:
 	safe_count = 0
-	row_number = 1
 	for i in file.readlines():
-		# if row_number > 20:
-		# 	break
 
 		problem_dampener = True
 		lst = [int(x) for x in i.split()]
 
-		if is_safe(row_number, problem_dampener, lst):
+		if is_safe(problem_dampener, lst):
 			safe_count += 1
 		else:
 			lst.pop(0)
-			problem_dampener = False
-			print(f"first run wasn't safe...trying to pop 0th index and try again with problem_dampener off")
-			if is_safe(row_number, problem_dampener, lst):
+			if is_safe(False, lst):
 				safe_count += 1
-
-		row_number += 1
 	
 	print(safe_count)
 			
