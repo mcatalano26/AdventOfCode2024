cc986fc5f8a1aa0ac3eeb9745fd6883603df13b3
Matt Catalano <mattcat26@gmail.com>
2024-12-15 21:11:22 -0600

    majorly clean up 2.2

--- Day2/p2.py
+++ Day2/p2.py
@@ -2,12 +2,14 @@
 
 from typing import List, Tuple
 
-def problem_dampener_run_setup(lst: List, ind: int) -> Tuple[List, List]:
+def problem_dampener_run(lst: List, ind: int) -> Tuple[List, List]:
 	lst1 = lst.copy()
 	lst2 = lst.copy()
 	lst1.pop(ind)
 	lst2.pop(ind-1)
-	return lst1, lst2
+	if is_safe(False, lst1): return True
+	elif is_safe(False, lst2): return True
+	else: return False
 
 def is_safe(problem_dampener: bool, lst: List) -> bool:
 	# check1 will check if our delta is at least 1 and at most 3
@@ -22,31 +24,16 @@ def is_safe(problem_dampener: bool, lst: List) -> bool:
 			continue
 		if abs(prev - curr) <= 3 and abs(prev - curr) >= 1:
 			check1 += 1
-		elif problem_dampener:
-			lst1, lst2 = problem_dampener_run_setup(lst, ind)
-			if is_safe(False, lst1): return True
-			elif is_safe(False, lst2): return True
-			else: return False
+		elif problem_dampener: return problem_dampener_run(lst, ind)
 		if prev-curr != 0:
-			if prev-curr < 0:
-				check2 += 1
-			if prev-curr > 0:
-				check2 -= 1
+			if prev-curr < 0: check2 += 1
+			if prev-curr > 0: check2 -= 1
 			if abs(check2) < ind:
-				if problem_dampener:
-					lst1, lst2 = problem_dampener_run_setup(lst, ind)
-					if is_safe(False, lst1): return True
-					elif is_safe(False, lst2): return True
-					else: return False
-		elif problem_dampener:
-			lst1, lst2 = problem_dampener_run_setup(lst, ind)
-			if is_safe(False, lst1): return True
-			elif is_safe(False, lst2): return True
-			else: return False
+				if problem_dampener: return problem_dampener_run(lst, ind)
+		elif problem_dampener: return problem_dampener_run(lst, ind)
 		prev = curr
 
-	if abs(check1) == len(lst)-1 and abs(check2) == len(lst)-1:
-		return True
+	if abs(check1) == len(lst)-1 and abs(check2) == len(lst)-1: return True
 	return False
 
 
@@ -54,15 +41,12 @@ with open("input.txt", "r") as file:
 	safe_count = 0
 	for i in file.readlines():
 
-		problem_dampener = True
 		lst = [int(x) for x in i.split()]
 
-		if is_safe(problem_dampener, lst):
-			safe_count += 1
-		else:
-			lst.pop(0)
-			if is_safe(False, lst):
-				safe_count += 1
+		if is_safe(True, lst): safe_count += 1; continue
+
+		lst.pop(0)
+		if is_safe(False, lst): safe_count += 1
 	
 	print(safe_count)
 			
